937515e8e121b13e1e2ef3c9abedcdb2fe536401
Spencer Markowski <spencer.markowski@gmail.com>
2010-11-21 06:59:42 -0500

    Adds Rails 3 support

--- lib/cable_menu/acts_as_cable.rb
+++ lib/cable_menu/acts_as_cable.rb
@@ -1,21 +1,21 @@
+require 'rails'
+
 module Cable
-  
+  class Railtie < Rails::Railtie
+    config.to_prepare do
+      ActiveRecord::Base.send(:include, Cable::ActsAsCable)
+    end
+  end
+end
+
+module Cable::ActsAsCable
   def self.included( base )
     base.send :extend, ClassMethods
   end
   
   module ClassMethods
     def acts_as_cable( reflection_options = {} )
-      send :include, InstanceMethods
       has_one :menu, reflection_options.merge( :as => :cable_menuable )
     end
   end
-  
-  module InstanceMethods
-    
-    
-  end
-  
-  ActiveRecord::Base.send :include, Cable
-  
 end
--- lib/cable_menu/acts_as_cable_menu.rb
+++ lib/cable_menu/acts_as_cable_menu.rb
@@ -1,17 +1,28 @@
+require 'rails'
+require 'awesome_nested_set'
+
 module CableMenu
+  class Railtie < Rails::Railtie
+    config.to_prepare do
+      ActiveRecord::Base.send(:include, CableMenu::ActsAsCableMenu)
+    end
+  end
+end
+
+module CableMenu::ActsAsCableMenu
   
   def self.included( base )
     base.send :extend, ClassMethods
   end
   
   module ClassMethods
+  
     def acts_as_cable_menu( reflection_options = {} )
       send :include, InstanceMethods
       belongs_to :cable_menuable, reflection_options.merge( :polymorphic => true )
       acts_as_nested_set
     end
   end
-  
   module InstanceMethods
     
     def resource
@@ -23,7 +34,5 @@ module CableMenu
     end
     
   end
-  
-  ActiveRecord::Base.send :include, CableMenu
   
 end
